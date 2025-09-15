23ec5d78eda836d0d50d0a7ceafd25f1db943451
gbengeneh <43642757+gbengeneh@users.noreply.github.com>
2025-09-15 13:29:28 +0100

    add htaccess commit

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,3 +1,18 @@
 <?php
  require_once __DIR__.'/../models/User.php';
  require_once __DIR__ .'/../utils/jwtUtil.php';
+
+ class AuthController{
+    private $user;
+
+    public function __construct(){
+        $this->user = new User();
+    }
+
+
+    public function register($data) {
+        if (empty($data['username']) || empty($data['email']) || empty($data['password'])){
+            htt
+        }
+    }
+ }
